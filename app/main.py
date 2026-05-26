9ecd66ffef7aa882f4fdbb02694975f896510f68
Tomas Gallo <tomas.gallo@mail.utdt.edu>
2026-05-26 10:29:04 -0300

    "prod"

--- app/main.py
+++ app/main.py
@@ -52,7 +52,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","http://10.1.100.3:4173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
